678da69d752d50ac41d7c26a3f63e2d60b4e7ad6
Girji <a.girjoaba@student.rug.nl>
2025-04-21 12:06:35 +0200

    anonimity of the authors in code

--- mcts_hsearch/src/mcts.h
+++ mcts_hsearch/src/mcts.h
@@ -1,26 +1,3 @@
-/**
- * @file mcts.h
- * @author Andrei Valentin Girjoaba (andrei.girjoaba02@gmail.com)
- * @brief 
- * @version 1.0
- * @date 2024-07-01
- * 
- * @copyright Copyright (C) 2024  Andrei Valentin Girjoaba
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
- * 
- */
 
 #ifndef MCTS_H
 #define MCTS_H
